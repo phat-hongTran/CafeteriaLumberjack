ca9855eea644456924a5563ac4b5df02a3606409
Phat Tran <ptran@strongtie.com>
2024-01-28 12:57:48 +0700

    Add project files.

---
+++ CafeteriaLumberjack/Lumberjack.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeteriaLumberjack
+{
+    class Lumberjack
+    {
+        private Random random = new Random();
+
+        public readonly string Name;
+        private Stack<Flapjack> flapjackStack = new Stack<Flapjack>();
+        public void TakeFlapjack(int number)
+        {
+            for(int i = 0; i < number; i++)
+            {
+                flapjackStack.Push((Flapjack)random.Next(4));
+            }
+        }
+
+        public void EatFlapjacks()
+        {
+            Console.WriteLine($"{Name} is eating flapjacks");
+            foreach (var item in flapjackStack)
+            {
+                Console.WriteLine($"{Name} ate a {item.ToString().ToLower()} flapjack");
+            }
+        }
+
+        public Lumberjack(string name)
+        {
+            Name = name;
+        }
+    }
+
+    enum Flapjack
+    {
+        Crispy,
+        Soggy,
+        Browned,
+        Banana,
+    }
+}
---
+++ CafeteriaLumberjack/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CafeteriaLumberjack
+{
+    class Program
+    {
+        public static Queue<Lumberjack> queue = new Queue<Lumberjack>();
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("First lumberjack's name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Number of flapjacks: ");
+            int.TryParse(Console.ReadLine(), out number);            
+
+            Lumberjack first = new Lumberjack(name);
+            first.TakeFlapjack(number);
+            queue.Enqueue(first);
+
+            while (true)
+            {
+                Console.WriteLine("Next lumberjack's name (blank to end): ");
+                name = Console.ReadLine();
+                if (string.IsNullOrEmpty(name)) break;
+                Console.WriteLine("Number of flapjacks: ");
+                int.TryParse(Console.ReadLine(), out number);
+
+                Lumberjack nextPerson = new Lumberjack(name);
+                nextPerson.TakeFlapjack(number);
+                queue.Enqueue(nextPerson);
+            }
+
+            while (queue.Any())
+            {
+                Lumberjack person = queue.Dequeue();
+                person.EatFlapjacks();
+            }
+        }
+    }
+}
